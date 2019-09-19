5d64c789123c2238d68b8ddc479e3272407e443f
whl <test@whl.com>
2019-09-19 23:09:58 +0800

    map

--- reliable-refund-response-manager/src/main/java/xarch/reliable/service/RefundInfoServer.java
+++ reliable-refund-response-manager/src/main/java/xarch/reliable/service/RefundInfoServer.java
@@ -3,7 +3,7 @@ package xarch.reliable.service;
 import java.util.Map;
 
 public interface RefundInfoServer {
-	public String setRefundInfo(String out_refund_no, Map<String, Object> refunddata);
+	public Map<String, Object> setRefundInfo(String out_refund_no, Map<String, Object> refunddata);
 
 	public Map<String, Object> getRefundInfo(String out_refund_no);
 
