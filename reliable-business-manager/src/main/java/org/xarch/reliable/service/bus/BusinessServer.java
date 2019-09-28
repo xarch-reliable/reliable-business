d1038585e0958ca04ca6867d0cf3f5479e4d28d7
wancy <wancy@wancy.com>
2019-09-28 14:00:41 +0800

    修正

--- reliable-business-manager/src/main/java/org/xarch/reliable/service/bus/BusinessServer.java
+++ reliable-business-manager/src/main/java/org/xarch/reliable/service/bus/BusinessServer.java
@@ -175,7 +175,7 @@ public class BusinessServer extends BusinessManager {
 			sendpayidmap.put("xrdataction", "getpayid");
 			Map<String, Object> payidmap = (Map<String, Object>)feignDataManager.doSupport2DataCenter(sendpayidmap).get("body");
 			
-			String payid = (String)payidmap.get(openid);
+			String payid = (String)payidmap.get("payid");
 			logger.info("[payid]=" + payid);
 			if (payid == null) {
 				resmap.put("error_msg", "payID获取失败");
